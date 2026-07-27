e4dda86317df34bb977bdbcf9e38d440a6d04f7b
Lisshanth S <lisshanth06@gmail.com>
2026-07-27 19:30:41 +0530

    Add/Update LeetCode solution: sort-array-by-parity

--- sort-array-by-parity/Solution.java
+++ sort-array-by-parity/Solution.java
@@ -1,27 +1,14 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int n = nums.length;
-        int[] arr = new int[n];
-        if(n==1){
-            return nums;
-        }
-        int i=0;
-        int k=0;
-        while(i<n){
-            if(nums[i]%2==0){
-                arr[k]=nums[i];
-                k++;
+        int slow = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                int temp = nums[slow];
+                nums[slow] = nums[i];
+                nums[i] = temp;
+                slow++;
             }
-            i++;
         }
-        int j=0;
-        while(j<n){
-            if(nums[j]%2!=0){
-                arr[k]=nums[j];
-                k++;
-            }
-            j++;
-        }
-        return arr;
+        return nums;
     }
 }
