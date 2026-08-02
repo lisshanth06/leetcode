44e4c6af153e28f4febcfceddb750c5ecded03c6
Lisshanth S <lisshanth06@gmail.com>
2026-08-02 12:05:48 +0530

    Used HashMap

--- majority-element/Solution.java
+++ majority-element/Solution.java
@@ -1,18 +1,13 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
-        int count=0;
-        for(int i=0;i<n;i++){
-            count =0;
-            for(int j=0;j<n;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }
-            if(count>n/2){
-                return nums[i];
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+            if(map.get(num)>n/2){
+                return num;
             }
         }
-        return 0;
+        return -1;
     }
 }
